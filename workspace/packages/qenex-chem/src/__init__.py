5bc898bf126d6b70016c1fd37f2fce716fbda9ce
root <ceo@qenex.ai>
2026-01-09 20:07:40 +0000

    refactor(qenex_chem): rename package and implement analytical gradients

--- workspace/packages/qenex-chem/src/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-QENEX Chemistry Package
-Domain Expert: Quantum Chemistry and Molecular Dynamics
-"""
-
-from molecule import Molecule
-from solver import HartreeFockSolver
-
-__all__ = ["Molecule", "HartreeFockSolver"]
